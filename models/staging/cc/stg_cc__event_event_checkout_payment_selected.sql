434675460db576424bdee873ad39edad80e51683
Daniel Wallace <dwallace@exomemory.net>
2021-06-21 13:34:50 -0700

    Added stg_cc__event_payment_selected.

---
+++ models/staging/cc/stg_cc__event_event_checkout_payment_selected.sql
@@ -0,0 +1,29 @@
+{{
+  config(
+    tags=["events"]
+  )
+}}
+
+with base as (
+  
+  select * from {{ ref('base_cc__ahoy_events') }}
+
+),
+
+event_checkout_initiated as (
+
+  select
+     event_id
+    ,visit_id
+    ,occurred_at_utc
+    ,user_id
+  from 
+    base
+  where 
+    event_name = 'custom_event'
+    and event_json:action::text = 'reached-step' 
+    and event_json:label::text = '1'
+
+)
+
+select * from event_checkout_initiated
