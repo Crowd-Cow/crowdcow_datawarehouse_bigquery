1c676158e38e812edd2d0b96479dff5e091b0f49
Killian <killian@crowdcow.com>
2021-06-21 08:38:33 -0600

    add stage model for cow cash

--- models/staging/stg_cc__cow_cash_entries.sql
+++ models/staging/stg_cc__cow_cash_entries.sql
@@ -7,28 +7,25 @@ with source as (
 renamed as (
 
     select
-        cow_cash_id,
-        user_id,
-        credit_id,
-        given_by_user_id,
-        cow_cash_message,
-        amount_frauded_in_cents,
-        amount_expired_in_cents,
-        amount_used_in_cents,
-        frauded_at,
-        entry_type,
-        gift_card_id,
-        finance_reporting_type,
-        updated_at,
-        cow_cash_entry_source_id,
-        order_id,
-        from_order_id,
-        created_at,
-        amount_in_cents,
-        expires_at,
-        _fivetran_deleted,
-        _fivetran_synced
-
+        id as cow_cash_id
+        ,user_id
+        ,credit_id
+        ,given_by_user_id
+        ,message as cow_cash_message
+        ,{{ cents_to_usd('amount_frauded_in_cents') }} as amount_frauded_usd
+        ,{{ cents_to_usd('amount_expired_in_cents') }} as amount_expired_usd
+        ,{{ cents_to_usd('amount_used_in_cents') }} as amount_used_usd
+        ,frauded_at as frauded_at_utc
+        ,entry_type
+        ,gift_card_id
+        ,finance_reporting_type
+        ,updated_at as updated_at_utc
+        ,cow_cash_entry_source_id
+        ,order_id
+        ,from_order_id
+        ,created_at as created_at_utc
+        ,{{ cents_to_usd('amount_in_cents') }} as cow_cash_amount_usd
+        ,expires_at as expires_at_utc
     from source
 
 )
