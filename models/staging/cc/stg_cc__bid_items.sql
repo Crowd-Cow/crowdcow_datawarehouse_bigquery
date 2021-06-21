54482a62cbbd296d52534d54df554994c59e9706
Morgan Tuff <morgan@crowdcow.com>
2021-06-21 12:34:21 -0700

    Yml file for bid_items and updated column aliases

--- models/staging/cc/stg_cc__bid_items.sql
+++ models/staging/cc/stg_cc__bid_items.sql
@@ -14,19 +14,19 @@ with source as (
 
   select 
     created_at as created_at_utc
-    , {{ clean_strings('description') }} as description
-    , {{ clean_strings('description_template') }} as description_template
+    , {{ clean_strings('description') }} as bid_item_description
+    , {{ clean_strings('description_template') }} as bid_item_description_template
     , event_id
     , hide_from_user as is_hidden_from_user
-    , id
-    , {{ clean_strings('item_photo_url') }} as item_photo_url
-    , {{ cents_to_usd('item_price_cents') }} as item_price_usd
-    , {{ clean_string('item_type') }} as item_type
-    , {{ clean_strings('name') }} as name
+    , id as bid_item_id
+    , {{ clean_strings('item_photo_url') }} as bid_item_photo_url
+    , {{ cents_to_usd('item_price_cents') }} as bid_item_price_usd
+    , {{ clean_strings('item_type') }} as bid_item_type
+    , {{ clean_strings('name') }} as bid_item_name
     , {{ clean_strings('portion_type') }} as portion_type 
     , quantity_available
     , {{ cents_to_usd('strike_through_price_cents') }} as strike_through_price_usd
-    , {{ clean_strings('subtype') }} as subtype
+    , {{ clean_strings('subtype') }} as bid_item_subtype
     , {{ clean_strings('token') }} as token
     , updated_at as updated_at_utc
     , product_variant_id
