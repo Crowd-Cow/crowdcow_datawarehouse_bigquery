ff10945ca0bfdae1dfe49164658c248b4df7d0ad
Daniel Wallace <dwallace@exomemory.net>
2021-06-21 13:23:16 -0700

    Added stg_cc__event_checkout_initiated

--- models/staging/cc/stg_cc__event_checkout_initiated.sql
+++ models/staging/cc/stg_cc__event_checkout_initiated.sql
@@ -17,9 +17,6 @@ event_checkout_initiated as (
     ,visit_id
     ,occurred_at_utc
     ,user_id
-    ,event_json:experiments     as experiments
-    ,event_json:member::boolean as is_member
-    ,event_json:value::text     as value
   from 
     base
   where 
