9e37bfed594726a7b272762e188488eafe38d262
Killian <killian@crowdcow.com>
2021-06-21 11:33:56 -0600

    fix column name in macro

--- macros/clean_strings.sql
+++ macros/clean_strings.sql
@@ -1,8 +1,8 @@
 {% macro clean_strings(column_name) %}
 
     case
-        when column_name = '' then null
-        else upper(trim(column_name))
+        when {{ column_name }} = '' then null
+        else upper(trim( {{ column_name }} ))
     end
-    
+
 {% endmacro %}
