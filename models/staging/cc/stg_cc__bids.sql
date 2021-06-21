6c13c447dfbb09225e9e66dba8816f083440ba20
Morgan Tuff <morgan@crowdcow.com>
2021-06-21 13:00:13 -0700

    Removing aliases

--- models/staging/cc/stg_cc__bids.sql
+++ models/staging/cc/stg_cc__bids.sql
@@ -13,34 +13,34 @@ with source as (
 , renamed as ( 
 
   select
-    s.id   as bid_id
-    , s.bid_item_id
-    , s.created_at as created_at_utc
-    , s.from_sms_log_entry_id
-    , s.from_user_mail_log_entry_id
-    , {{ cents_to_usd('s.item_price_cents') }} as item_price_usd
-    , s.mix_portion_group_ids
-    , s.order_id
-    , s.quantity
-    , s.updated_at as updated_at_utc
-    , s.user_id
-    , s.product_id
-    , s.promotion_id
-    , {{ clean_strings('s.name')  }} as name
-    , {{ clean_strings('s.description') }} as description
-    , {{ clean_strings('s.item_photo_url') }} as item_photo_url
-    , s.subscription_id
-    , s.custom_subscription_item_id
-    , {{ clean_strings('s.token') }} as token
-    , {{ clean_strings('s.reason') }} as reason
-    , s.target_sku_id
-    , s.fill_score
-    , s.reserve_inventory_immediately as is_reserve_inventory_immediately
-    , {{ clean_strings('s.fill_type') }} as fill_type
-    , s.fulfillment_at_risk as is_fulfillment_at_risk
-    , s.product_permutation_id
-    , s.target_product_permutation_id
-    , s.first_stuck_at as first_stuck_at_utc
+    id   as bid_id
+    , bid_item_id
+    , created_at as created_at_utc
+    , from_sms_log_entry_id
+    , from_user_mail_log_entry_id
+    , {{ cents_to_usd('item_price_cents') }} as item_price_usd
+    , mix_portion_group_ids
+    , order_id
+    , quantity
+    , updated_at as updated_at_utc
+    , user_id
+    , product_id
+    , promotion_id
+    , {{ clean_strings('name')  }} as name
+    , {{ clean_strings('description') }} as description
+    , {{ clean_strings('item_photo_url') }} as item_photo_url
+    , subscription_id
+    , custom_subscription_item_id
+    , {{ clean_strings('token') }} as token
+    , {{ clean_strings('reason') }} as reason
+    , target_sku_id
+    , fill_score
+    , reserve_inventory_immediately as is_reserve_inventory_immediately
+    , {{ clean_strings('fill_type') }} as fill_type
+    , fulfillment_at_risk as is_fulfillment_at_risk
+    , product_permutation_id
+    , target_product_permutation_id
+    , first_stuck_at as first_stuck_at_utc
 
     from source as s
 
