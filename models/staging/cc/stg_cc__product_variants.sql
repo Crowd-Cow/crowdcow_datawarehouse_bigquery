f4ef23fd6b23aed433068ee0dedf62315b2464ff
Morgan Tuff <morgan@crowdcow.com>
2021-06-23 11:11:55 -0700

    stg_cc__product_variants and yml file

---
+++ models/staging/cc/stg_cc__product_variants.sql
@@ -0,0 +1,33 @@
+
+with source as (
+
+    select * from {{ source('cc', 'product_variants') }}
+
+),
+
+renamed as (
+
+    select
+      id as product_variant_id
+      , {{ cents_to_usd('min_product_bundle_value_in_cents') }} as min_product_bundle_value_in_usd
+      , fc_id 
+      , {{ clean_strings('highlight_text') }} as product_variant_highlight_text
+      , strike_through_percent_off
+      , {{ clean_strings('description') }} as description
+      , created_at as created_at_utc
+      , {{ cents_to_usd('max_product_bundle_value_in_cents') }} as max_product_bundle_value_in_usd
+      , {{ clean_strings('automated_highlight_text_type') }} as product_variant_automated_highlight_text_type
+      , {{ cents_to_usd('price_in_cents') }} as price_in_usd
+      , product_id
+      , updated_at as updated_at_utc
+      , image_url as product_variant_image_url
+      , can_subscribe_to_variant 
+      , always_available as is_product_variant_always_available
+      , _fivetran_deleted
+      , _fivetran_synced 
+
+    from source
+
+)
+
+select * from renamed
