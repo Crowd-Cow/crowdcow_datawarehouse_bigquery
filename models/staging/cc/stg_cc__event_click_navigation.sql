a0d321af07f48aa6290aa181de1f95329e6e3bb6
killian-dent <killian.dent@gmail.com>
2021-06-25 14:14:40 -0600

    add click navigation stg model

---
+++ models/staging/cc/stg_cc__event_click_navigation.sql
@@ -0,0 +1,29 @@
+{{
+    config(
+        tags = ["events"]
+    )
+}}
+
+with base as (
+
+    select * from {{ ref('base_cc__ahoy_events') }}
+),
+
+event_click_navigation as (
+
+    select
+        event_id
+        ,visit_id
+        ,occurred_at_utc
+        ,user_id
+        ,trim({{ clean_strings('event_json:label::text') }},'\n') as navigation_label
+    from
+        base
+    where
+        event_name = 'custom_event'
+            and event_json:category::text = 'navigation'
+            and event_json:action::text = 'click-navigation'
+
+)
+
+select * from event_click_navigation
