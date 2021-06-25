7b5dbc2c7783aabede0af5faf263c5163e725458
killian-dent <killian.dent@gmail.com>
2021-06-25 14:32:35 -0600

    rename references

--- models/datawarehouse/ecommerce/intermediate/int_visit_events__unioned.sql
+++ models/datawarehouse/ecommerce/intermediate/int_visit_events__unioned.sql
@@ -23,7 +23,7 @@ event_visits as ( select * from {{ ref('stg_cc__event_visits') }} )
         ,null as event_id
         ,'visit_start' as event_name
         ,started_at_utc as occurred_at_utc
-    from stg_cc__event_visits
+    from event_visits
 )
 ,checkout_initiated as (
     select
@@ -32,7 +32,7 @@ event_visits as ( select * from {{ ref('stg_cc__event_visits') }} )
         ,event_id
         ,'checkout_initiated' as event_name
         ,occurred_at_utc
-    from stg_cc__event_checkout_initiated
+    from event_checkout_initiated
 )
 ,checkout_payment_selected as (
     select
@@ -41,7 +41,7 @@ event_visits as ( select * from {{ ref('stg_cc__event_visits') }} )
         ,event_id
         ,'checkout_payment_selected' as event_name
         ,occurred_at_utc
-    from stg_cc__event_checkout_payment_selected
+    from event_checkout_payment_selected
 )
 ,order_complete as (
     select
@@ -50,7 +50,7 @@ event_visits as ( select * from {{ ref('stg_cc__event_visits') }} )
         ,event_id
         ,'order_complete' as event_name
         ,occurred_at_utc
-    from stg_cc__event_order_complete
+    from event_order_complete
 )
 ,order_paid as (
     select
@@ -59,7 +59,7 @@ event_visits as ( select * from {{ ref('stg_cc__event_visits') }} )
         ,event_id
         ,'order_paid' as event_name
         ,occurred_at_utc
-    from stg_cc__event_order_paid
+    from event_order_paid
 )
 ,page_view as (
     select
@@ -69,7 +69,7 @@ event_visits as ( select * from {{ ref('stg_cc__event_visits') }} )
         ,'page_view: ' ||
             split_part(parse_url(page_viewed_url):path::text,'/',1) as event_name
         ,occurred_at_utc
-    from stg_cc__event_page_view
+    from event_page_view
 )
 ,pdp_added_to_cart as (
     select
@@ -78,7 +78,7 @@ event_visits as ( select * from {{ ref('stg_cc__event_visits') }} )
         ,event_id
         ,'pdp_added_to_cart' as event_name
         ,occurred_at_utc
-    from stg_cc__event_pdp_added_to_cart
+    from event_pdp_added_to_cart
 )
 ,viewed_product as (
     select
@@ -87,7 +87,7 @@ event_visits as ( select * from {{ ref('stg_cc__event_visits') }} )
         ,event_id
         ,'viewed_product: ' || bid_item_name as event_name
         ,occurred_at_utc
-    from stg_cc__event_viewed_product
+    from event_viewed_product
 )
 ,click_navigation as (
     select
@@ -96,7 +96,7 @@ event_visits as ( select * from {{ ref('stg_cc__event_visits') }} )
         ,event_id
         ,'click_navigation: ' || navigation_label as event_name
         ,occurred_at_utc
-    from stg_cc__event_click_navigation
+    from event_click_navigation
 )
 ,union_events as (
     select * from visit_events
