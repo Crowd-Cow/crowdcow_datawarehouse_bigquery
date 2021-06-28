dcf6d6a9318058b2cb37f6a64994a3332a0c98c2
killian-dent <killian.dent@gmail.com>
2021-06-28 10:00:47 -0600

    change event order

--- models/datawarehouse/ecommerce/fact_visit.sql
+++ models/datawarehouse/ecommerce/fact_visit.sql
@@ -25,7 +25,7 @@ aggregate_events as (
 
     select
         visit_id
-        ,array_agg(event_name) within group (order by event_id, occurred_at_utc)::variant as visit_event_sequence
+        ,array_agg(event_name) within group (order by occurred_at_utc, event_id)::variant as visit_event_sequence
     from small_events
     group by 1
 
