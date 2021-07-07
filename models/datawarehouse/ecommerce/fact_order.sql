e14f78e5ded96be91e25367a9e1a68271fa26df3
Morgan Tuff <morgan@crowdcow.com>
2021-07-06 18:31:58 -0700

    Adding rank logic

--- models/datawarehouse/ecommerce/fact_order.sql
+++ models/datawarehouse/ecommerce/fact_order.sql
@@ -4,4 +4,48 @@ with stage as (
 
 )
 
-select * from stage
+, order_rank as (
+    select user_id
+        , order_paid_at_utc
+        , order_id
+        , rank () over (partition by user_id order by order_paid_at_utc, order_id ) all_order_rank
+    from stage 
+    where order_cancelled_at_utc is null 
+        and order_paid_at_utc is not null 
+        and stripe_failure_code is null 
+)
+, subscription_rank as ( 
+    select user_id
+        , order_paid_at_utc
+        , order_id
+        , rank () over (partition by user_id order by order_paid_at_utc, order_id ) as subscription_order_rank
+    from stage 
+    where order_cancelled_at_utc is null 
+        and order_paid_at_utc is not null 
+        and stripe_failure_code is null
+        and subscription_id is not null
+)
+
+, alacarte_rank as ( 
+    select user_id
+        , order_paid_at_utc
+        , order_id
+        , rank () over (partition by user_id order by order_paid_at_utc, order_id ) as alc_order_rank
+    from stage 
+    where order_cancelled_at_utc is null 
+        and order_paid_at_utc is not null 
+        and stripe_failure_code is null
+        and subscription_id is null
+)
+, all_up_order_ranks as ( 
+    select stage.* 
+        , order_rank.all_order_rank
+        , subscription_rank.subscription_order_rank
+        , alacarte_rank.alc_order_rank
+    from stage
+    left join order_rank on order_rank.order_id = stage.order_id 
+    left join subscription_rank on subscription_rank.order_id = stage.order_id
+    left join alacarte_rank on alacarte_rank.order_id = stage.order_id
+)
+
+select * from all_up_order_ranks 
