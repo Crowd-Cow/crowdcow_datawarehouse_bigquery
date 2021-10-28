15a157036db1c645d44ff160db3c4d4f8d138c08
killian-dent <killian.dent@gmail.com>
2021-10-28 11:35:14 -0600

    add bid item sku quantity stage

---
+++ models/staging/cc/stg_cc__bid_item_sku_with_quantities.sql
@@ -0,0 +1,22 @@
+with source as (
+
+    select * from {{ source('cc', 'bid_item_sku_with_quantities') }} where not _fivetran_deleted
+
+),
+
+renamed as (
+
+    select
+        id as bid_item_sku_quantity_id
+        , sku_id
+        , updated_at as updated_at_utc
+        , created_at as created_at_utc
+        , quantity as sku_quantity
+        , bid_item_id
+
+    from source
+
+)
+
+select * from renamed
+
