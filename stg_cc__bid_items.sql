14abe438963bcbcba18a0416e7044b285505853f
killian-dent <killian.dent@gmail.com>
2021-10-28 16:05:41 -0600

    add order items yml

--- stg_cc__bid_items.sql
+++
@@ -1,4 +0,0 @@
-Running with dbt=0.20.0
-Encountered an error:
-Compilation Error in model order_items (models/business_vault/order_items.sql)
-  Model 'model.cc_datawarehouse.order_items' (models/business_vault/order_items.sql) depends on a node named 'stg_cc__bid_item' which was not found
