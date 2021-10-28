184c5edd66f2754a46ab9af0049802b56b70a3f2
Morgan Tuff <morgan@crowdcow.com>
2021-10-28 14:32:18 -0700

    staging, yml and snapshot for sku_boxes

---
+++ models/staging/cc/stg_cc__sku_boxes.sql
@@ -0,0 +1,43 @@
+with source as (
+
+    select * from {{ ref('sku_boxes_ss') }}
+
+),
+
+renamed as (
+
+    select
+        id as sku_box_id,
+        fc_id as fulfillment_center_id,
+        sku_id,
+        marked_destroyed_at as marked_destroyed_at_utc,
+        quantity,
+        max_weight,
+        quarantined_quantity,
+        created_at as created_at_utc,
+        {{ clean_strings('name') }} as name,
+        fc_location_parent_id,
+        marked_not_for_sale_at as marked_not_for_sale_at_utc,
+        updated_at as updated_at_utc,
+        quantity_reserved,
+        barcode,
+        delivered_at as delivered_at_utc,
+        pallet_id,
+        min_weight,
+        moved_to_picking_at as moved_to_picking_at_utc,
+        owner_id,
+        lot_id,
+        _fivetran_deleted,
+        manually_queued_for_on_deck,
+        manually_queued_for_picking,
+        scanned,
+        filled,
+        printed,
+        _fivetran_synced
+
+    from source
+
+)
+
+select * from renamed
+
