cb08ec52d6143247f28e45c1de9cf46b7648615d
killian-dent <killian.dent@gmail.com>
2021-10-29 14:39:40 -0600

    add farm and vendor tag stages

---
+++ models/staging/cc/stg_cc__vendor_tags.sql
@@ -0,0 +1,22 @@
+with source as (
+
+    select * from {{ source('cc', 'vendor_tags') }} where not _fivetran_deleted
+
+),
+
+renamed as (
+
+    select
+        id as vendor_tag_id
+        ,{{ clean_strings('key') }} as vendor_tag_key
+        ,updated_at as updated_at_utc
+        ,{{ clean_strings('value') }} as vendor_tag_value
+        ,created_at as created_at_utc
+        ,{{ clean_strings('description') }} as vendor_tag_description
+
+    from source
+
+)
+
+select * from renamed
+
