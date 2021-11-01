b7b1837a98ab0c5ecb11273053587ece53e7fc92
killian-dent <killian.dent@gmail.com>
2021-11-01 14:13:17 -0600

    add order packed sku yml

--- models/staging/cc/stg_cc__order_packed_skus.sql
+++ models/staging/cc/stg_cc__order_packed_skus.sql
@@ -8,7 +8,7 @@ renamed as (
 
     select
         id as order_packed_sku_id
-        ,name as order_packed_sku_name
+        ,{{ clean_strings('name') }} as order_packed_sku_name
         ,{{cents_to_usd('payment_processing_fee_in_cents') }} as payment_processing_fee_usd
         ,order_id
         ,sku_id
@@ -22,8 +22,8 @@ renamed as (
         ,{{ cents_to_usd('platform_fee_in_cents') }} as platform_fee_usd
         ,{{ cents_to_usd('fulfillment_fee_in_cents') }} as fulfillment_fee_usd
         ,updated_at as updated_at_utc
-        ,reason
-        ,farm_name
+        ,{{ clean_strings('reason') }} as reason
+        ,{{ clean_strings('farm_name') }} as farm_name
         ,user_id
         ,committed_at as committed_at_utc
         ,{{ cents_to_usd('cost_in_cents') }} as sku_cost_usd
