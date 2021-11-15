2cf308ae8e639c37d3b44f6e4f61aee59179c8ca
killian-dent <killian.dent@gmail.com>
2021-11-15 16:02:14 -0700

    add autofill business logic and yml

---
+++ models/business_vault/autofill_bids.sql
@@ -0,0 +1,31 @@
+with
+
+bid_log as ( select * from {{ ref('stg_cc__autofill_bid_logs') }} )
+,autofill_order as ( select * from {{ ref('stg_cc__autofill_order_logs') }} )
+
+,autofill_joins as (
+    select
+        bid_log.autofill_bid_log_id
+        ,bid_log.target_sku_id
+        ,bid_log.target_product_permutation_id
+        ,bid_log.autofill_sku_id
+        ,bid_log.autofill_product_permutation_id
+        ,autofill_order.order_id
+        ,autofill_order.previous_order_id
+        ,bid_log.bid_id
+        ,bid_log.target_sku_name
+        ,bid_log.target_quantity
+        ,bid_log.autofill_sku_name
+        ,bid_log.autofill_quantity
+        ,autofill_order.autofill_type
+        ,bid_log.reason
+        ,autofill_order.notes
+        ,autofill_order.filled_at_date
+        ,bid_log.created_at_utc
+        ,bid_log.updated_at_utc
+    from bid_log
+        left join autofill_order on bid_log.autofill_order_log_id = autofill_order.autofill_order_log_id
+)
+
+select * from autofill_joins
+    
