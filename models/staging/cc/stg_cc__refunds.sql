c7bed958ae4280ac375aa1bbf96e3e29fa510206
killian-dent <killian.dent@gmail.com>
2021-12-10 14:40:42 -0700

    change refunds to use stripe

--- models/staging/cc/stg_cc__refunds.sql
+++ models/staging/cc/stg_cc__refunds.sql
@@ -1,6 +1,6 @@
 with source as (
 
-    select * from {{ source('cc', 'refunds') }} where not _fivetran_deleted
+    select * from {{ source('stripe', 'refund') }}
 
 ),
 
@@ -8,15 +8,14 @@ renamed as (
 
     select
         id as refund_id
+        ,{{ cents_to_usd('amount') }} as refund_amount_usd
+        ,created as created_at_utc
+        ,{{ clean_strings('currency') }} as currency
         ,{{ clean_strings('reason') }} as refund_reason
-        ,{{ clean_strings('stripe_balance_transaction') }} as stripe_balance_transaction_id
-        ,{{ clean_strings('stripe_refund_id') }} as stripe_refund_id
-        ,created_at as created_at_utc
-        ,updated_at as updated_at_utc
-        ,refund_created_at as refund_created_at_utc
-        ,{{ clean_strings('stripe_charge_id') }} as stripe_charge_id
-        ,order_id
-        ,{{ cents_to_usd('amount_in_cents') }} as refund_amount_usd
+        ,receipt_number
+        ,{{ clean_strings('status') }} as refund_status
+        ,{{ clean_strings('balance_transaction_id') }} as stripe_balance_transaction_id
+        ,{{ clean_strings('charge_id') }} as stripe_charge_id
 
     from source
 
