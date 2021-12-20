6ec537ae88cf15e011bd5c27d1cf9cf5003a54a0
killian-dent <killian.dent@gmail.com>
2021-12-20 15:04:48 -0700

    add credit group

--- models/business_vault/credits.sql
+++ models/business_vault/credits.sql
@@ -49,4 +49,49 @@ credit as ( select * from {{ ref('stg_cc__credits') }} )
     from add_cow_cash_information
         left join promotion on add_cow_cash_information.promotion_id = promotion.promotion_id
 )
-select * from add_promotion_info
+
+,group_credit_descriptions as (
+    select
+        credit_id
+        ,promotion_id
+        ,user_id
+        ,order_id
+        ,cow_cash_entry_source_id
+        ,credit_type
+        ,credit_description
+
+        ,case
+            when (credit_description like '%LATE%' and credit_description not like '%RELATED%')
+                or credit_description like any ('%DELAY%','%ARRIVED LATE%','%DELIVERY FAILURE%') then 'LATE SHIPMENT'
+            when credit_description like any ('%LOST%','%MISSING ORDER%','%NEVER ARRIVE%','%UNDELIVER%','%NON-DELIVER%') then 'LOST ORDER'
+            when credit_description like '%THAW%' then 'THAWED'
+            when credit_description like any ('%INVENTORY%','%INR%','%OOS%','%MISSING_ITEM%','%ITEM MISSING%','%MISSING FROM%') then  'MISSING ITEM'
+            when credit_description like '%LEAK%' then 'LEAKER'
+            when credit_description = 'REPLACEMENT'
+                or credit_description like any ('REPLACEMENT ORDER%','REPLACEMENT 2%','REPLACEMENT FOR%') then 'REPLACEMENT'
+            when credit_description like any ('%NEW SUB%','%FIRST SUB%','%1ST SUB%','%SUBSCRIPTION PRICE%','%SUBSCRIBER DISCOUNT%','%SUBSCRIPTION DISCOUNT%'
+                ,'%MEMBER DISCOUNT%','%MEMBERSHIP DISCOUNT%','%MEMBER PRICING%','%MEMBER PRICE%','%MEMBERSHIP PRICING%','%MEMBERSHIP PRICE%','%SUBSCRIPTION PRICING%') then 'MEMBER PRICE FIX'
+            when credit_description like any ('%GB PROMO%','%BACON PROMO%','%HONORING PROMO%','%BEEF PROMO%','%WAGYU PROMO%','%FREE PROMO%') then 'PRODUCT PROMO'
+            when credit_description like '%QUALITY%' then 'QUALITY ISSUE'
+            when credit_description like '%MISSING_PROMO%' then 'MISSING PROMO'
+            when credit_description like '%MATCH%' then 'PRICE MATCH'
+            when credit_description like any ('%FEDEX%','%DAMAGE%','%AXLEHIRE%') then 'CARRIER ISSUE'
+            when credit_description like any ('%ACCIDENTAL SUBSCRIPTION%','%UNINTENDED SUBSCRIPTION%','%NOT WANT SUB%') then 'DID NOT WANT MEMBERSHIP'
+            when credit_description like any ('%MARKETING%','%INFLUENCER%','%AMBASSADOR%') then 'MARKETING'
+            when credit_description like '%WHOLESALE%' then 'WHOLESALE DISCOUNT'
+         end as credit_group
+         
+        ,awarded_cow_cash_entry_type
+        ,awarded_cow_cash_message
+        ,promotion_type
+        ,credit_discount_usd
+        ,discount_percent
+        ,is_hidden_from_user
+        ,is_controlled_by_promotion
+        ,is_new_member_promotion
+        ,created_at_utc
+        ,updated_at_utc
+    from add_promotion_info
+)
+
+select * from group_credit_descriptions
