97bbba46f436ff82ffff5c6c6e128252fa37319b
killian-dent <killian.dent@gmail.com>
2022-01-05 12:23:00 -0700

    add item discounts

--- models/business_vault/order_items.sql
+++ models/business_vault/order_items.sql
@@ -8,6 +8,7 @@ bids as ( select * from {{ ref('stg_cc__bids') }} )
         bids.order_id
         ,bids.bid_id
         ,bids.bid_item_id
+        ,bids.promotion_id
         ,bids.bid_token
         ,bids.product_id
         ,bids.product_name
@@ -18,8 +19,8 @@ bids as ( select * from {{ ref('stg_cc__bids') }} )
         ,bids.fill_type
         ,coalesce(bids.bid_list_price_usd,bids.item_price_usd) as bid_list_price_usd
         ,coalesce(bids.item_price_usd, bids.bid_price_paid_usd) as bid_price_paid_usd
-        ,bids.bid_non_member_price_usd
-        ,bids.bid_member_price_usd
+        ,coalesce(bids.bid_non_member_price_usd,bids.item_price_usd) as bid_non_member_price_usd
+        ,coalesce(bids.bid_member_price_usd,bids.item_price_usd) as bid_member_price_usd
         ,bids.bid_quantity
         ,bids.created_at_utc
         ,bids.updated_at_utc
@@ -35,6 +36,7 @@ bids as ( select * from {{ ref('stg_cc__bids') }} )
         order_id
         ,bid_id
         ,bid_item_id
+        ,promotion_id
         ,bid_token
         ,product_id
         ,product_name
@@ -46,7 +48,7 @@ bids as ( select * from {{ ref('stg_cc__bids') }} )
         ,bid_list_price_usd
         ,bid_price_paid_usd
         ,bid_list_price_usd * bid_quantity as order_item_revenue
-        ,(bid_list_price_usd - bid_price_paid_usd) * bid_quantity as order_item_discount
+        ,(bid_list_price_usd - bid_price_paid_usd) * bid_quantity as total_order_item_discount
         ,bid_non_member_price_usd
         ,bid_member_price_usd
         ,bid_quantity
@@ -56,4 +58,46 @@ bids as ( select * from {{ ref('stg_cc__bids') }} )
     from order_item_joins
 )
 
-select * from order_item_revenue_calculations
+,order_item_discount_calculations as (
+    select
+        order_id
+        ,bid_id
+        ,bid_item_id
+        ,promotion_id
+        ,bid_token
+        ,product_id
+        ,product_name
+        ,bid_item_name
+        ,bid_item_type
+        ,bid_item_subtype
+        ,autofill_reason
+        ,fill_type
+        ,bid_list_price_usd
+        ,bid_price_paid_usd
+        ,order_item_revenue
+        ,total_order_item_discount
+
+        ,round(
+            case
+                when total_order_item_discount > 0 then bid_list_price_usd * .05 -- Standard member discount is 5%
+                else 0
+            end
+        ,2) as item_member_discount
+    
+        ,round(
+            case
+                when total_order_item_discount > 0 then total_order_item_discount - (bid_list_price_usd * .05) -- Anything above the standard 5% member discount should be considered a merch discount
+                else 0
+            end
+        ,2) as item_merch_discount
+
+        ,bid_non_member_price_usd
+        ,bid_member_price_usd
+        ,bid_quantity
+        ,created_at_utc
+        ,updated_at_utc
+        ,first_stuck_at_utc
+    from order_item_revenue_calculations
+)
+
+select * from order_item_discount_calculations
