7788d2d504609416dce76fa70e62b9f4f5719bf6
killian-dent <killian.dent@gmail.com>
2022-01-10 16:25:17 -0700

    fix null forecasts

--- models/staging/forecasting/stg_forecasting__cat_subcat_daily.sql
+++ models/staging/forecasting/stg_forecasting__cat_subcat_daily.sql
@@ -9,10 +9,10 @@ renamed as (
     select
         {{ dbt_utils.surrogate_key(['item_id','date']) }} as forecast_id
         ,item_id
-        ,split_part(item_id,'--',1) as fc_id
-        ,split_part(item_id,'--',2) as category
-        ,split_part(item_id,'--',3) as sub_category
-        ,split_part(item_id,'--',4) as cut_id
+        ,split_part(item_id,'--',1)::int as fc_id
+        ,{{ clean_strings("split_part(item_id,'--',2)") }} as category
+        ,{{ clean_strings("split_part(item_id,'--',3)") }} as sub_category
+        ,split_part(item_id,'--',4)::int as cut_id
         ,date as forecast_date
         ,p10
         ,p50
@@ -20,6 +20,7 @@ renamed as (
         ,p90
 
     from source
+    where item_id <> 'null'
 
 )
 
