6f95886f3454c862becf0ad7f43e8e16f6a80f67
emurphy22 <34039457+emurphy22@users.noreply.github.com>
2022-01-21 12:11:17 -0800

    commit

--- models/staging/cc/stg_ccpa_requests.sql
+++ models/staging/cc/stg_ccpa_requests.sql
@@ -1,8 +1,9 @@
 
+
 select
    {{ clean_strings('first_name') }} as first_name
   ,{{ clean_strings('last_name') }} as last_name
-  ,email_address as email
+  ,{{ clean_strings('email_address')}} as email
   ,admin_link
   ,date_received::date as date_received
 from {{ ref('stg_strings__ccpa_requests') }}
