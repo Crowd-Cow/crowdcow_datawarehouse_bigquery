c7998c5de4f6c21be27eb706df7479f6828cbe44
killian-dent <killian.dent@gmail.com>
2022-01-27 16:59:35 -0700

    add free protein to discounts and rev

--- models/business_vault/discounts.sql
+++ models/business_vault/discounts.sql
@@ -32,7 +32,7 @@ credit as ( select * from {{ ref('credits') }} )
         ,case
             when discounts.index = 0 then 'MEMBERSHIP 5%'
             when discounts.index = 1 then 'MERCHANDISING DISCOUNT'
-            when discounts.index = 2 and promotion_id in (18,20,22) then 'MEMBERSHIP PROMOTIONS'
+            when discounts.index = 2 and promotion_id in (18,20,22) then 'MEMBERSHIP FREE PROTEIN PROMOTIONS'
             when discounts.index = 2 and promotion_id not in (18,20,22) then 'OTHER ITEM LEVEL PROMOTIONS'
          end as business_group
 
