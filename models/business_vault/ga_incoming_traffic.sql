b2233f74b08a44cb1f5ae43752a03b4e42142a68
emurphy22 <34039457+emurphy22@users.noreply.github.com>
2022-02-17 10:04:10 -0800

    renamed google_analytics to ga

--- models/business_vault/ga_incoming_traffic.sql
+++ models/business_vault/ga_incoming_traffic.sql
@@ -1,5 +1,5 @@
 with
 
-ga_incoming_traffic as ( select * from {{ ref('stg_google_analytics__incoming_traffic') }} )
+ga_incoming_traffic as ( select * from {{ ref('stg_ga__incoming_traffic') }} )
 
 select * from ga_incoming_traffic
