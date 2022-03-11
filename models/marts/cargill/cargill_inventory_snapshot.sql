1839d31988bf5dec7f01d79a5181aa065fa80f10
killian-dent <killian.dent@gmail.com>
2022-03-11 14:21:32 -0700

    create cargill mart and add views

---
+++ models/marts/cargill/cargill_inventory_snapshot.sql
@@ -0,0 +1,9 @@
+with
+
+inventory as ( select * from {{ ref('inventory_snapshot') }} )
+,sku as ( select * from {{ ref('skus') }} )
+
+select inventory.*
+from inventory
+    left join sku on inventory.sku_key = sku.sku_key
+where sku.is_cargill
