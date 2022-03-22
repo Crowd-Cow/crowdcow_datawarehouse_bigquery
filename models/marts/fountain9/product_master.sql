d13efee5c055e4cdd66040abcdc9530f6b6c9f88
killian-dent <killian.dent@gmail.com>
2022-03-22 12:25:47 -0600

    add inventory classification

--- models/marts/fountain9/product_master.sql
+++ models/marts/fountain9/product_master.sql
@@ -9,8 +9,15 @@ select
     ,sub_category
     ,sku_id
     ,sku_name
-    ,is_always_in_stock
+    
+    ,case
+        when is_always_in_stock then 'AIS'
+        else 'OTHER'
+     end as inventory_classification
+    
     ,sku_vendor_name as brand
-    ,'units' as uom
+    ,'UNITS' as uom
 from sku
 where dbt_valid_to is null
+    and cut_id is not null
+    and category is not null
