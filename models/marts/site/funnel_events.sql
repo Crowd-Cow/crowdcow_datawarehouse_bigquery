55904af95146b35c170611800a8050c79874f73f
emurphy22 <34039457+emurphy22@users.noreply.github.com>
2022-04-21 13:39:42 -0700

    true_funnel

--- models/marts/site/funnel_events.sql
+++ models/marts/site/funnel_events.sql
@@ -4,7 +4,7 @@ event_groupings as ( select * from {{ ref('events') }} )
 
 select distinct event_groupings.visit_id
     ,count_if(event_groupings.event_type = 'VIEWED SLIDEOUT CART') > 0 and count_if(event_groupings.event_type = 'VIEWED ADDRESS PAGE') > 0 as slideout_and_address_page
-    ,count_if(event_groupings.event_type = 'VIEWED ADDRESS PAGE') > 0 and count_if(event_groupings.event_type = 'VIEWED PAYMENT PAGE') > 0 as address_page_and_payment_page
-    ,count_if(event_groupings.event_type = 'VIEWED PAYMENT PAGE') > 0 and count_if(event_groupings.event_type = 'CHECKOUT COMPLETE') > 0 as payment_page_and_checkout_complete
+    ,count_if(event_groupings.event_type = 'VIEWED SLIDEOUT CART') > 0 and count_if(event_groupings.event_type = 'VIEWED ADDRESS PAGE') > 0 and count_if(event_groupings.event_type = 'VIEWED PAYMENT PAGE') > 0 as address_page_and_payment_page
+    ,count_if(event_groupings.event_type = 'VIEWED SLIDEOUT CART') > 0 and count_if(event_groupings.event_type = 'VIEWED ADDRESS PAGE') > 0 and count_if(event_groupings.event_type = 'VIEWED PAYMENT PAGE') > 0 and count_if(event_groupings.event_type = 'CHECKOUT COMPLETE') > 0 as payment_page_and_checkout_complete
 from event_groupings
 group by 1
