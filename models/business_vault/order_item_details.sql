6af8709be227f34142308c26031a775edf8d29e7
killian-dent <killian.dent@gmail.com>
2022-04-22 18:38:33 -0600

    update vendor owner marketplace logic

--- models/business_vault/order_item_details.sql
+++ models/business_vault/order_item_details.sql
@@ -118,7 +118,7 @@ ordered_items as ( select * from {{ ref('int_ordered_skus') }} )
     as sku_net_product_revenue
     
     ,is_item_packed
-    ,is_marketplace
+    ,sku_owner_id <> 91 as is_marketplace
     ,is_single_sku_bid_item
     ,created_at_utc
     ,updated_at_utc
