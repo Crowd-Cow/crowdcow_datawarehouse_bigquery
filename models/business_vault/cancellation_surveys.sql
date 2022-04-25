e6ff61740d9909bf5c97fc43a8e49138569c92d6
killian-dent <killian.dent@gmail.com>
2022-04-25 16:16:43 -0600

    add cancellation surveys

---
+++ models/business_vault/cancellation_surveys.sql
@@ -0,0 +1,19 @@
+with
+
+cancel_events as ( select * from {{ ref('stg_cc__events') }} where event_name = 'BRIGHTBACK_CANCEL' )
+
+select
+    event_id
+    ,visit_id
+    ,user_id
+    ,user_token
+    ,subscription_token
+    ,occurred_at_utc
+    ,brightback_id
+    ,session_id
+    ,session_key
+    ,display_reason
+    ,feedback
+    ,selected_reason
+from cancel_events 
+qualify row_number() over ( partition by brightback_id order by occurred_at_utc desc ) = 1
---
+++ models/staging/cc/stg_cc__subscription_promotions.sql
@@ -0,0 +1,16 @@
+with
+
+sub_promo as ( select * from {{ source('cc', 'subscription_promotions') }} where not _fivetran_deleted)
+
+,renamed as (
+    select
+        id as subscription_promotion_id
+        ,updated_at as updated_at_utc
+        ,created_at as created_at_utc
+        ,subscription_id
+        ,promotion_id
+        ,promotion_selection_id
+    from sub_promo
+)
+
+select * from renamed
