b2aecd5e3a966ae40318ed81f17ba03bcf59a0fb
killian-dent <killian.dent@gmail.com>
2022-05-04 16:05:45 -0600

    acumatica invoice adjustments

--- models/business_vault/acumatica_invoices.sql
+++ models/business_vault/acumatica_invoices.sql
@@ -6,40 +6,38 @@ select
     bill_item_id
     ,invoice_key
     ,bill_id
-    ,bill_date_utc
+    ,reference_number
+    ,location_id
+    ,vendor
+    ,vendor_ref
+    ,account_nbr
+    ,sub_account
+    ,currency_id
     ,notes
-    ,due_date_utc
     ,cash_account
     ,bill_description
-    ,regexp_substr(bill_description,'[0-9]{4}') as lot_number
     ,bill_type
 
      ,case 
-        when bill_type = 'BILL' then bill_amount
-        when bill_type = 'CREDIT ADJ.' then bill_amount
         when bill_type = 'DEBIT ADJ.' then -bill_amount
         when bill_type = 'PREPAYMENT' then null
+        else bill_amount
      end as bill_amount
 
-    ,reference_number
-    ,location_id
-    ,hold
     ,terms
-    ,vendor
-    ,is_approved_for_payment
-    ,vendor_ref
-    ,currency_id
     ,post_period
     ,bill_status
-    ,account_nbr
     ,line_item_amount
     ,branch
     ,line_item_description
     ,line_item_extended_cost
     ,line_item_quantity
-    ,sub_account
     ,transaction_description
     ,unit_cost
     ,line_item_note
     ,line_number
+    ,is_on_hold
+    ,is_approved_for_payment
+    ,bill_date_utc
+    ,due_date_utc
 from invoice
