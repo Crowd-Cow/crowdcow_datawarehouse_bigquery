d0751864283ea3cb9fe5a3c59dec2f5784fb844c
emurphy22 <34039457+emurphy22@users.noreply.github.com>
2022-05-06 09:46:54 -0700

    updates

--- models/business_vault/intermediate/int_order_cost.sql
+++ models/business_vault/intermediate/int_order_cost.sql
@@ -6,34 +6,20 @@ order_item as ( select * from {{ ref('order_item_details') }} )
 ,packaging_cost as (select * from {{ ref('int_packaging_cost_per_order')}} )
 ,care_cost as (select * from {{ ref('int_care_cost_per_order') }} )
 
-,order_cost_aggregation as (
-    select
-        order_id
-        ,sum(sku_cost) as product_cost
-    from order_item
-    group by 1
-)
-
-,static_gross_margin_costs as (
+,combined_costs as (
     select 
         orders.order_id
         ,order_coolant_cost
         ,order_packaging_cost
         ,order_care_cost
+        ,sum(order_item.sku_cost) as product_cost
     from orders
         left join coolant_cost on orders.order_id = coolant_cost.order_id
         left join packaging_cost on orders.order_id = packaging_cost.order_id
         left join care_cost on orders.order_id = care_cost.order_id
+        left join order_item on orders.order_id = order_item.order_id
+    group by 1, 2, 3, 4
 )
 
-,combined_costs as (
-    select static_gross_margin_costs.order_id
-        ,static_gross_margin_costs.order_coolant_cost
-        ,static_gross_margin_costs.order_packaging_cost
-        ,static_gross_margin_costs.order_care_cost
-        ,order_cost_aggregation.product_cost
-    from static_gross_margin_costs
-        left join order_cost_aggregation on static_gross_margin_costs.order_id = order_cost_aggregation.order_id
-)
 
 select * from combined_costs
