fa26172c70e08a895110488b72ce506af086c2c6
killian-dent <killian.dent@gmail.com>
2022-05-09 17:09:32 -0600

    fix null issue

--- models/staging/google_sheets/stg_gs__fc_care_packaging_costs.sql
+++ models/staging/google_sheets/stg_gs__fc_care_packaging_costs.sql
@@ -4,7 +4,7 @@ source as ( select * from {{ source('google_sheets', 'fc_care_packaging_costs') 
 
 ,renamed as (
     select {{ clean_strings('FULFILLMENT_CENTER_NAME') }} as fc_name
-        ,fc_id_auto_populated::number as fc_id
+        ,nullif(fc_id_auto_populated,'NULL')::number as fc_id
         ,MONTH::timestamp as month_of_costs  
         ,{{ clean_strings('BOX_TYPE') }} as box_type
         ,COST_USD::float as cost_usd
