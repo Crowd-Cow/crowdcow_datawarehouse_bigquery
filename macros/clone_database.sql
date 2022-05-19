4bc7e413efc23a82ba8bb2a37d9fef79db14f680
killian-dent <killian.dent@gmail.com>
2022-05-19 14:58:18 -0600

    fix clone issue

--- macros/clone_database.sql
+++ macros/clone_database.sql
@@ -1,6 +1,6 @@
 {% macro clone_database(db_name, db_to_clone) %}
 
-    {% set sql="create database {} clone {}".format(db_name, db_to_clone) %}
+    {% set sql="create or replace database {} clone {}".format(db_name, db_to_clone) %}
     {% do run_query(sql) %}
     {{ log("{} database cloned from {}".format(db_name, db_to_clone), info=True) }}
 
