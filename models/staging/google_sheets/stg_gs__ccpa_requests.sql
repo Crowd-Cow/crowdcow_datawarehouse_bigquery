ab9e570865cdf8b28d5dec848aacfc2bcb322c14
killian-dent <killian.dent@gmail.com>
2022-06-06 14:41:00 -0600

    fix date parse on ccpa staging model

--- models/staging/google_sheets/stg_gs__ccpa_requests.sql
+++ models/staging/google_sheets/stg_gs__ccpa_requests.sql
@@ -22,7 +22,7 @@ format_columns as (
     ,{{ clean_strings('last_name') }} as last_name
     ,{{ clean_strings('email_address')}} as email
     ,{{ clean_strings('admin_link') }} as admin_link
-    ,to_date(date_received,'mm/dd/yy') as date_received
+    ,to_date(date_received,'mm/dd/yyyy') as date_received
   from format_columns
 )
 
