6914a7dec79fab87eaa5ccfb718b5977dc500fe1
emurphy22 <34039457+emurphy22@users.noreply.github.com>
2022-06-16 13:50:19 -0700

    mart_for_user_attribution

---
+++ models/marts/marketing/user_attribution.sql
@@ -0,0 +1,18 @@
+with visits as ( select * from {{ ref('visit_classification') }} )
+,users as ( select user_id, attributed_visit_id from {{ ref('users') }} )
+
+,attribution_details as (
+    select visits.visit_id
+        ,users.user_id
+        ,visits.utm_source
+        ,visits.utm_medium
+        ,visits.utm_campaign
+        ,visits.utm_content
+        ,visits.utm_term
+        ,visits.channel
+        ,visits.sub_channel
+    from visits
+    join users on users.attributed_visit_id = visits.visit_id
+)
+
+select * from attribution_details
