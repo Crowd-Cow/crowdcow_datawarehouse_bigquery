5167c5eb4910550665e808ada890a1bfabb7eee6
killian-dent <killian.dent@gmail.com>
2022-06-21 09:44:20 -0600

    set up staging model for fountain9 forecast

---
+++ models/staging/fountain9/stg_fountain9__demand_forecasts.sql
@@ -0,0 +1,20 @@
+with
+
+source as ( select * from {{ ref('demand_forecast_ss') }} where dbt_valid_to is null )
+
+,renamed as (
+    select
+        id as forecast_id
+        ,date as forecast_date
+        ,fc_id
+        ,{{ clean_strings('fc_name') }} as fc_name
+        ,{{ clean_strings('category') }} as category
+        ,{{ clean_strings('sub_category') }} as sub_category
+        ,cut_id
+        ,{{ clean_strings('cut_name') }} as cut_name
+        ,{{ clean_strings('inventory_classification') }} as inventory_classification
+        ,predicted_quantity as forecasted_sales
+    from source
+)
+
+select * from renamed
