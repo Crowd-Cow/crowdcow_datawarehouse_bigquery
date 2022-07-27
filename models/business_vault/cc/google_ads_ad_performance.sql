43df3af612dadb2be068d5bdcdccc20bfab8bc52
killian-dent <killian.dent@gmail.com>
2022-07-27 15:06:04 -0600

    clean up campaign history

--- models/business_vault/cc/google_ads_ad_performance.sql
+++ models/business_vault/cc/google_ads_ad_performance.sql
@@ -1,10 +1,13 @@
-with ad_stats as (select * from {{ ref('stg_google_ads__ad_stats')}})
-    ,ad_history as (select * from {{ ref('stg_google_ads__ad_history')}})
-    ,campaign_history as (select * from {{ ref('stg_google_ads__campaign_history')}} )
-    ,ad_group_history as (select * from {{ ref('stg_google_ads__ad_group_history')}})
+with 
+
+ad_stats as (select * from {{ ref('stg_google_ads__ad_stats')}})
+,ad_history as (select * from {{ ref('stg_google_ads__ad_history')}})
+,campaign_history as (select * from {{ ref('stg_google_ads__campaign_history')}} )
+,ad_group_history as (select * from {{ ref('stg_google_ads__ad_group_history')}})
 
 ,ad_clicks_cost as (
-    select ad_id
+    select 
+        ad_id
         ,ad_group_id
         ,campaign_id
         ,date_utc
@@ -15,10 +18,11 @@ with ad_stats as (select * from {{ ref('stg_google_ads__ad_stats')}})
         ,sum(conversions_value) as total_conversion_value
     from ad_stats
     group by 1, 2, 3, 4
-    )
+)
 
 ,ad_url as (
-    select ad_id
+    select 
+        ad_id
         ,trim(trim(final_urls,']'),'[') as final_url
         ,updated_at_utc::date as ad_url_valid_from_date
         ,ifnull(lead(updated_at_utc::date,1) over(partition by ad_id order by updated_at_utc),'2999-01-01') as ad_url_valid_to_date
@@ -26,42 +30,50 @@ with ad_stats as (select * from {{ ref('stg_google_ads__ad_stats')}})
 )
 
 ,campaign_info as (
-    select campaign_id
+    select 
+        campaign_id
         ,campaign_name
-        ,case when campaign_name like '%SHOPPING%' then 'SHOPPING' else campaign_name end as campaign_grouping
-        ,updated_at_utc::date as campaign_valid_from_date
-        ,ifnull(lead(updated_at_utc::date,1) over(partition by campaign_id order by updated_at_utc),'2999-01-01') as campaign_valid_to_date
+        
+        ,case 
+            when campaign_name like '%SHOPPING%' then 'SHOPPING' 
+            else campaign_name 
+         end as campaign_grouping
+        
+        ,campaign_valid_from_date
+        ,campaign_valid_to_date
     from campaign_history
 )
 
 ,ad_group_details as (
-select ad_group_id
-    ,ad_group_name
-    ,updated_at_utc::date as ad_group_valid_from_date
-    ,ifnull(lead(updated_at_utc::date,1) over(partition by ad_group_id order by updated_at_utc),'2999-01-01') as ad_group_valid_to_date
-from ad_group_history
+    select 
+        ad_group_id
+        ,ad_group_name
+        ,updated_at_utc::date as ad_group_valid_from_date
+        ,ifnull(lead(updated_at_utc::date,1) over(partition by ad_group_id order by updated_at_utc),'2999-01-01') as ad_group_valid_to_date
+    from ad_group_history
 )
 
-    select distinct ad_clicks_cost.ad_id
-        ,ad_clicks_cost.ad_group_id
-        ,ad_clicks_cost.campaign_id
-        ,ad_clicks_cost.date_utc
-        ,ad_clicks_cost.total_clicks
-        ,ad_clicks_cost.total_impressions
-        ,ad_clicks_cost.total_cost_usd
-        ,ad_clicks_cost.total_conversions
-        ,ad_clicks_cost.total_conversion_value
-        ,ad_url.final_url
-        ,campaign_info.campaign_name
-        ,ad_group_details.ad_group_name
-        ,{{ dbt_utils.surrogate_key( ['ad_clicks_cost.date_utc','campaign_grouping'] ) }} as campaign_key
-    from ad_clicks_cost
-         left join ad_url on ad_clicks_cost.ad_id = ad_url.ad_id
-                          and ad_clicks_cost.date_utc >= ad_url.ad_url_valid_from_date
-                          and ad_clicks_cost.date_utc < ad_url.ad_url_valid_to_date
-        left join campaign_info on ad_clicks_cost.campaign_id = campaign_info.campaign_id
-                           and ad_clicks_cost.date_utc >= campaign_info.campaign_valid_from_date
-                           and ad_clicks_cost.date_utc < campaign_info.campaign_valid_to_date
-        left join ad_group_details on ad_clicks_cost.ad_group_id = ad_group_details.ad_group_id
-                           and ad_clicks_cost.date_utc >= ad_group_details.ad_group_valid_from_date
-                           and ad_clicks_cost.date_utc < ad_group_details.ad_group_valid_to_date
+select distinct 
+    ad_clicks_cost.ad_id
+    ,ad_clicks_cost.ad_group_id
+    ,ad_clicks_cost.campaign_id
+    ,ad_clicks_cost.date_utc
+    ,ad_clicks_cost.total_clicks
+    ,ad_clicks_cost.total_impressions
+    ,ad_clicks_cost.total_cost_usd
+    ,ad_clicks_cost.total_conversions
+    ,ad_clicks_cost.total_conversion_value
+    ,ad_url.final_url
+    ,campaign_info.campaign_name
+    ,ad_group_details.ad_group_name
+    ,{{ dbt_utils.surrogate_key( ['ad_clicks_cost.date_utc','campaign_grouping'] ) }} as campaign_key
+from ad_clicks_cost
+    left join ad_url on ad_clicks_cost.ad_id = ad_url.ad_id
+        and ad_clicks_cost.date_utc >= ad_url.ad_url_valid_from_date
+        and ad_clicks_cost.date_utc < ad_url.ad_url_valid_to_date
+    left join campaign_info on ad_clicks_cost.campaign_id = campaign_info.campaign_id
+        and ad_clicks_cost.date_utc >= campaign_info.campaign_valid_from_date
+        and ad_clicks_cost.date_utc < campaign_info.campaign_valid_to_date
+    left join ad_group_details on ad_clicks_cost.ad_group_id = ad_group_details.ad_group_id
+        and ad_clicks_cost.date_utc >= ad_group_details.ad_group_valid_from_date
+        and ad_clicks_cost.date_utc < ad_group_details.ad_group_valid_to_date
--- models/staging/google_ads/stg_google_ads__campaign_history.sql
+++ models/staging/google_ads/stg_google_ads__campaign_history.sql
@@ -10,8 +10,7 @@ source as ( select * from {{ source('google_ads', 'campaign_history') }} )
     select
         id as campaign_id
         ,updated_at as updated_at_utc
-        ,updated_at::date as campaign_valid_from_date
-        ,ifnull(lead(updated_at::date,1) over(partition by campaign_id order by updated_at),'2999-01-01') as campaign_valid_to_date
+        ,updated_at::date as updated_at_date
         ,customer_id
         ,base_campaign_id
         ,{{ clean_strings('ad_serving_optimization_status') }} as ad_serving_optimization_status
@@ -39,8 +38,7 @@ source as ( select * from {{ source('google_ads', 'campaign_history') }} )
     select
         id
         ,updated_at as updated_at_utc
-        ,updated_at::date as campaign_valid_from_date
-        ,ifnull(lead(updated_at::date,1) over(partition by id order by updated_at),'2999-01-01') as campaign_valid_to_date
+        ,updated_at::date as updated_at_date
         ,customer_id
         ,base_campaign_id
         ,{{ clean_strings('ad_serving_optimization_status') }} as ad_serving_optimization_status
@@ -64,4 +62,45 @@ source as ( select * from {{ source('google_ads', 'campaign_history') }} )
 
 )
 
-select * from renamed
+,dedup_history as (
+    select
+        *
+    from renamed
+    qualify row_number() over(partition by campaign_id, updated_at_date order by updated_at_utc desc) = 1
+)
+
+,get_snapshot_dates as (
+    select 
+        campaign_id
+        ,customer_id
+        ,base_campaign_id
+        ,ad_serving_optimization_status
+        ,advertising_channel_subtype
+        ,advertising_channel_type
+        ,experiment_type
+        ,end_date
+        ,final_url_suffix
+        ,frequency_caps
+        ,campaign_name
+        ,optimization_score
+        ,payment_mode
+        ,serving_status
+        ,start_date
+        ,campaign_status
+        ,tracking_url_template
+        ,vanity_pharma_display_url_mode
+        ,vanity_pharma_text
+        ,video_brand_safety_suitability
+        ,updated_at_utc
+        ,{{ dbt_utils.surrogate_key(['campaign_id','updated_at_date']) }} as history_key
+
+        ,case
+            when updated_at_date = first_value(updated_at_date) over(partition by campaign_id order by updated_at_date) then '1970-01-01'
+            else updated_at_date
+         end as campaign_valid_from_date
+        
+        ,ifnull(lead(updated_at_date) over(partition by campaign_id order by updated_at_date),'2999-01-01') as campaign_valid_to_date
+    from dedup_history
+)
+
+select * from get_snapshot_dates
