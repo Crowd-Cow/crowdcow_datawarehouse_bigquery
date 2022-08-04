3a0bd362366ff3ce131a3fe707783a5e7323c98f
killian-dent <killian.dent@gmail.com>
2022-08-04 15:20:52 -0600

    fix snapshot to date

--- models/business_vault/cc/inventory_snapshot.sql
+++ models/business_vault/cc/inventory_snapshot.sql
@@ -41,11 +41,10 @@ dates as ( select calendar_date from {{ ref('stg_reference__date_spine') }} wher
         ,delivered_at_utc
         ,moved_to_picking_at_utc
         ,dbt_valid_from::date as dbt_valid_from
-        ,coalesce(dbt_valid_to,sysdate())::date as dbt_valid_to
+        ,coalesce(dbt_valid_to,sysdate())::date + 1 as dbt_valid_to
         ,row_number() over(partition by sku_box_id, dbt_valid_from::date order by dbt_valid_from desc) as rn
     from sku_box
     qualify rn = 1
-    order by dbt_valid_from
 )
 
 ,daily_sku_boxes as (
