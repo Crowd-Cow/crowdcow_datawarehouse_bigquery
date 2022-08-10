6d7af289be93277876b12265ee7ac09c9630fec0
killian-dent <killian.dent@gmail.com>
2022-08-09 18:06:49 -0600

    refactor filter events

--- models/marts/site/filter_events.sql
+++ models/marts/site/filter_events.sql
@@ -2,74 +2,59 @@ with
 
 filter_event as ( select * from {{ ref('events') }} where event_name = 'PRODUCT-FILTER-SORT-CHANGED' )
 
-,hash_filters as (
+,compare_filters as (
     select
         event_id
-        ,occurred_at_utc
         ,visit_id
         ,user_id
-        ,event_name
+        ,occurred_at_utc
         ,from_filter
         ,to_filter
-        ,md5(hash(object_keys(from_filter))) as from_key_hash
-        ,md5(hash(object_keys(to_filter))) as to_key_hash
-        ,md5(hash(from_filter)) as from_hash
-        ,md5(hash(to_filter)) as to_hash
+        ,parse_json(datawarehouse.compare_objects(from_filter,to_filter)) as filter_changes
     from filter_event
 )
 
-,split_out_filters as (
-    select
-        coalesce(from_filters.event_id,to_filters.event_id) as event_id
-        ,coalesce(from_filters.occurred_at_utc,to_filters.occurred_at_utc) as occurrred_at_utc
-        ,coalesce(from_filters.visit_id,to_filters.visit_id) as visit_id
-        ,coalesce(from_filters.user_id,to_filters.user_id) as user_id
-        ,coalesce(from_filters.event_name,to_filters.event_name) as event_name
-        ,coalesce(from_filters.did_filter_value_change,to_filters.did_filter_value_change) as did_filter_value_change
-        ,coalesce(from_filters.did_filter_key_change,to_filters.did_filter_key_change) as did_filter_key_change
-        ,from_filters.from_filter_key
-        ,from_filters.from_filter_value
-        ,to_filters.to_filter_key
-        ,to_filters.to_filter_value
-    from 
-    (
-        select
-            event_id
-            ,occurred_at_utc
-            ,visit_id
-            ,user_id
-            ,event_name
-            ,from_key_hash <> to_key_hash as did_filter_key_change
-            ,from_hash <> to_hash as did_filter_value_change
-            ,from_filter
-            ,to_filter
-            ,regexp_replace(frm.path::text,'[\\[\\]\']','') as from_filter_key
-            ,frm.value::text as from_filter_value
-        from hash_filters,
-            lateral flatten( input => from_filter) frm
-    ) as from_filters
+,parse_filter_changes as (
+    select 
+        compare_filters.event_id
+        ,compare_filters.visit_id
+        ,compare_filters.user_id
+        ,compare_filters.occurred_at_utc
+        ,modification.key
+        ,modification.value::text as filter_value
+        ,modification.path
+        
+        ,case
+            when key is null then regexp_replace(path,'(added_filter|modified_filter)|([\'.\\[\\]0-9])','')
+            else key
+        end as path_clean
     
-    full outer join 
-    
-    (
-        select
-            event_id
-            ,occurred_at_utc
-            ,visit_id
-            ,user_id
-            ,event_name
-            ,from_key_hash <> to_key_hash as did_filter_key_change
-            ,from_hash <> to_hash as did_filter_value_change
-            ,from_filter
-            ,to_filter
-            ,regexp_replace(to_f.path::text,'[\\[\\]\']','') as to_filter_key
-            ,to_f.value::text as to_filter_value
-        from hash_filters,
-            lateral flatten( input => to_filter) to_f
-    ) as to_filters 
-    
-    on from_filters.event_id = to_filters.event_id
-            and from_filters.from_filter_key = to_filters.to_filter_key
+    from compare_filters,
+        lateral flatten( input => filter_changes, recursive => true ) as modification
+    where path like any ('added_filter%','modified_filter%')
+        {% raw %} and not(value like any ('[%]','{%}')) {% endraw %}
 )
 
-select * from split_out_filters
+
+,clean_filter_actions as (
+    select distinct
+        event_id
+        ,visit_id
+        ,user_id
+        ,occurred_at_utc
+
+        ,case 
+            when path like 'added_filter%' then 'ADDED FILTER'
+            when path like 'modified_filter%' then 'MODIFIED FILTER'
+            else 'OTHER'
+        end as filter_action
+
+        ,case
+            when path_clean like any ('%[min]%','%[max]%') then upper(replace(replace(path_clean,'[','_'),']',''))
+            else upper(regexp_replace(path_clean,'[\\[\\]]',''))
+         end as filter_name
+        ,upper(coalesce((try_cast(filter_value as int)/100)::text,filter_value)) as filter_value
+    from parse_filter_changes
+)
+
+select * from clean_filter_actions
