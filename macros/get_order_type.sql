0a780df95f898a3c95b943f7b7b1d700f98bc4f6
killian-dent <killian.dent@gmail.com>
2022-08-10 14:29:31 -0600

    update macro

--- macros/get_order_type.sql
+++ macros/get_order_type.sql
@@ -1,7 +1,8 @@
 {%- macro get_order_type(table_name) -%}
+    
     (
         select max( order_type ) as order_type 
-            from business_vault.orders
+            from staging.stg_cc__orders
             where orders.order_id = {{ table_name }}.order_id
     )
   
