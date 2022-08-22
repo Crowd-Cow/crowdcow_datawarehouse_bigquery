bec632eac165ef9e0bec08a7e0540402aa668407
killian-dent <killian.dent@gmail.com>
2022-08-22 14:22:00 -0600

    fix list

--- models/marts/marketing/google_ads_customer_lists.sql
+++ models/marts/marketing/google_ads_customer_lists.sql
@@ -10,9 +10,11 @@ user as ( select * from {{ ref('users') }} )
         ,user.first_name
         ,user.last_name
         ,'us' as country
+        ,user.state_code as user_state
         ,user.user_zip
         ,user.is_lead as is_customer_lead
         ,true as is_lead_and_registered_user
+        ,user.lifetime_paid_order_count > 0 as is_customer
         ,user.last_90_days_paid_order_count > 0 as is_standard_customer
         ,user.last_90_days_paid_order_count > 0 and user.is_purchasing_customer as is_active_non_member_90_day
         ,user.is_active_member_90_day and not is_cancelled_member as is_active_member_90_day
@@ -28,10 +30,12 @@ user as ( select * from {{ ref('users') }} )
         ,trim(lower(first_name)) as first_name
         ,trim(lower(last_name)) as last_name
         ,country
+        ,trim(lower(user_state)) as state
         ,trim(user_zip) as zip
         ,trim(phone_number) as phone_number
         ,is_customer_lead
         ,is_lead_and_registered_user
+        ,is_customer
         ,is_standard_customer
         ,is_vip_customer
         ,is_active_non_member_90_day
@@ -45,10 +49,12 @@ user as ( select * from {{ ref('users') }} )
         ,first_name
         ,last_name
         ,country
+        ,state
         ,zip
         ,iff(regexp_like(phone_number,'\\([0-9]{3}\\) [0-9]{3}-[0-9]{4}'),'1 ' || phone_number,null) as phone_number
         ,is_customer_lead
         ,is_lead_and_registered_user
+        ,is_customer
         ,is_standard_customer
         ,is_vip_customer
         ,is_active_non_member_90_day
