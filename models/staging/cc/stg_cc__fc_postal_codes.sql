4c23bf359d44b5a17a94c607306cac70df645757
killian-dent <killian.dent@gmail.com>
2022-09-16 13:18:33 -0600

    use source fc postal codes

--- models/staging/cc/stg_cc__fc_postal_codes.sql
+++ models/staging/cc/stg_cc__fc_postal_codes.sql
@@ -1,6 +1,6 @@
 with
 
-source as ( select * from {{ ref('fc_postal_codes_ss') }} where not _fivetran_deleted )
+source as ( select * from {{ source('cc', 'fc_postal_codes') }} where not _fivetran_deleted )
 
 ,renamed as (
   select
