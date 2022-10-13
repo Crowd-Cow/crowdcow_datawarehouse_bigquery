4e0101715a3421e91bc4ee043906dd7cefa39cdd
killian-dent <killian.dent@gmail.com>
2022-10-13 09:48:50 -0600

    add adjusted net rev and gross profit

--- models/marts/users/user_ltv.sql
+++ models/marts/users/user_ltv.sql
@@ -9,11 +9,37 @@ purchasing_user as ( select * from {{ ref('users') }} where customer_cohort_date
         ,is_rastellis
         ,user_id
         ,order_paid_at_utc
-        ,net_product_revenue - product_cost as product_profit
+        ,product_profit
         ,div0(product_profit,gross_product_revenue) as product_margin
-        ,net_revenue - product_Cost - shipment_cost - packaging_cost - payment_processing_cost - coolant_cost
-            - care_cost - fc_labor_cost - poseidon_fulfillment_cost as gross_profit
+        ,gross_profit
         ,div0(gross_profit,net_revenue) as gross_margin
+        ,net_revenue
+        
+        ,round(
+            gross_product_revenue 
+            + membership_discount 
+            + merch_discount
+            + free_protein_promotion
+            + item_promotion
+            + shipping_revenue 
+            + free_shipping_discount
+            {# + new_member_discount #}
+            + refund_amount
+            + gift_redemption
+            + other_discount
+        ,2) as adjusted_net_revenue
+        
+        ,adjusted_net_revenue 
+            - product_cost 
+            - shipment_cost 
+            - packaging_cost 
+            - payment_processing_cost
+            - coolant_cost 
+            - care_cost 
+            - fc_labor_cost 
+            - poseidon_fulfillment_cost 
+            - inbound_shipping_cost as adjusted_gross_profit
+            
     from paid_orders
 )
 
@@ -31,5 +57,8 @@ select
     ,calc_margin.product_margin
     ,calc_margin.gross_profit
     ,calc_margin.gross_margin
+    ,calc_margin.net_revenue
+    ,calc_margin.adjusted_net_revenue
+    ,calc_margin.adjusted_gross_profit
 from purchasing_user
     left join calc_margin on purchasing_user.user_id = calc_margin.user_id
