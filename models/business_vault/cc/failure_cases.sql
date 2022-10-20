808da272514d91bc99a232b8306801378070c9fd
killian-dent <killian.dent@gmail.com>
2022-10-20 09:47:17 -0600

    add standard category to failure cases

--- models/business_vault/cc/failure_cases.sql
+++ models/business_vault/cc/failure_cases.sql
@@ -2,4 +2,18 @@ with
 
 failure_case as ( select * from {{ ref('stg_cc__failure_cases') }} )
 
-select * from failure_case
+select 
+    *
+    ,case 
+            when specifics like any ('%QUALITY%') then 'PRODUCT QUALITY'
+            when specifics like any ('%MISSING ITEM%') then 'MISSING ITEM'
+            when specifics like any ('%CUSTOMER SERVICE%','%SUPPORT%') then 'CUSTOMER SERVICE'
+            when specifics like any ('%DELIVERY%LATE%','%SHIPMENT%LATE%','%NEVER%ARRIVED%','%LOST%') then 'DELIVERY LATE'
+            when specifics like any ('%LEAK%') then 'LEAKER'
+            when specifics like any ('%RESCHEDULE%') or category = 'RESCHEDULED ORDER' then 'RESCHEDULE'
+            when specifics like any ('%THAW%') then 'THAW'
+            when specifics like any ('%REMOVED%','%NO%INVENTORY%') then 'REMOVED ITEMS'
+            else 'OTHER'
+        end as standard_category
+        
+from failure_case
