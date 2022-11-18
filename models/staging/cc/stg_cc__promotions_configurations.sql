2de7cd400bc07dc146d41abffcfa25f9c55bf946
killian-dent <killian.dent@gmail.com>
2022-11-17 17:22:47 -0700

    add promo configuration stage

---
+++ models/staging/cc/stg_cc__promotions_configurations.sql
@@ -0,0 +1,17 @@
+with
+
+source as ( select * from {{ source('cc', 'promotions_configurations') }} )
+
+,renamed as (
+    select
+        id as promotion_configuration_id
+        ,{{ clean_strings('key') }} as promotion_configuration_key
+        ,created_at as created_at_utc
+        ,{{ clean_strings('value') }} as promotion_configuration_value
+        ,{{ clean_strings('configurable_type') }} as configurable_type
+        ,configurable_id
+        ,updated_at as updated_at_utc
+    from source
+)
+
+select * from renamed
