0957dec2eb211d3c8208c6aac3c4caefc5a9c0e4
emurphy22 <34039457+emurphy22@users.noreply.github.com>
2023-02-16 14:55:33 -0800

    pick_pack

--- models/business_vault/cc/pick_pack_durations.sql
+++ models/business_vault/cc/pick_pack_durations.sql
@@ -1,9 +1,17 @@
 with
 
 pick_pack as ( select * from {{ ref('int_pick_pack_durations') }} )
+,order_details as (select order_id, is_rastellis, is_qvc from {{ ref('stg_cc__orders')}})
 
+,combined_pick_pack_orders as (
 select 
-    {{ dbt_utils.surrogate_key(['user_id','order_id','action','action_date']) }} as packing_action_id
-    ,{{ get_order_type('pick_pack') }} as is_rastellis
-    ,* 
+    {{ dbt_utils.surrogate_key(['user_id','pick_pack.order_id','action','action_date']) }} as packing_action_id
+    ,order_details.is_rastellis
+    ,order_details.is_qvc
+    ,pick_pack.* 
 from pick_pack
+    left join order_details on pick_pack.order_id = order_details.order_id
+)
+
+select *
+from combined_pick_pack_orders
