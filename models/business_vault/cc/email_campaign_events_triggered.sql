f1e139874ced99bbda93c4f8c73b0a14e9f0e078
Daniel Bolivar <daniel.bolivar@crowdcow.com>
2023-07-21 15:25:15 -0500

    fixed query for triggered emails

--- models/business_vault/cc/email_campaign_events_triggered.sql
+++ models/business_vault/cc/email_campaign_events_triggered.sql
@@ -1,20 +1,34 @@
 with 
 email_campaign_events as ( select * from {{ ref('email_campaign_events') }} )
 
-,emails as (
+
+,max_ended AS (
     SELECT 
-        user_id
-        ,campaign_name
-        ,max(ended_at_utc)
-        ,sum(send_count) as send_count
-        ,sum(open_count) as open_count
-        ,sum(click_count) as click_count
-        ,sum(unique_open_count) as unique_open_count
-        ,sum(bounce_count) as bounce_count
-    from email_campaign_events 
-    where
-        campaign_type = 'TRIGGERED' 
-    GROUP by 
-        1,2 
+        user_id,
+        campaign_name,
+        MAX(ended_at_utc) AS max_ended_at_utc
+    FROM email_campaign_events 
+    WHERE campaign_type = 'TRIGGERED' 
+    GROUP BY user_id, campaign_name
 )
-select * from emails 
+,final as (
+    SELECT 
+        ROW_NUMBER() OVER(ORDER BY max_ended.user_id, max_ended.campaign_name, max_ended.max_ended_at_utc) AS prim_key,
+        max_ended.user_id,
+        max_ended.campaign_name,
+        max_ended_at_utc as ended_at_utc,
+        SUM(send_count) AS send_count,
+        SUM(open_count) AS open_count,
+        SUM(click_count) AS click_count,
+        SUM(unique_open_count) AS unique_open_count,
+        SUM(bounce_count) AS bounce_count
+    FROM max_ended
+    JOIN email_campaign_events AS events 
+        ON max_ended.user_id = events.user_id 
+        AND max_ended.campaign_name = events.campaign_name 
+        AND max_ended.max_ended_at_utc = events.ended_at_utc
+    GROUP BY max_ended.user_id, max_ended.campaign_name, max_ended.max_ended_at_utc
+    ORDER BY max_ended.user_id, max_ended.campaign_name, max_ended.max_ended_at_utc
+)
+
+select * from final 
