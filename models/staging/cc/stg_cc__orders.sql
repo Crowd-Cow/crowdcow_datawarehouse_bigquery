8c898c2322efc018c3891808e4cff7a8808d5f07
Daniel Bolivar <daniel.bolivar@crowdcow.com>
2023-08-11 10:52:34 -0500

    Ignore order created by mistake

--- models/staging/cc/stg_cc__orders.sql
+++ models/staging/cc/stg_cc__orders.sql
@@ -1,6 +1,6 @@
 with 
 
-source as ( select * from {{ source('cc', 'orders') }} where not _fivetran_deleted)
+source as ( select * from {{ source('cc', 'orders') }} where not _fivetran_deleted and id != 2871732)
 ,postal_code as ( select * from {{ ref('stg_cc__postal_codes') }} )
 
 ,renamed as (
