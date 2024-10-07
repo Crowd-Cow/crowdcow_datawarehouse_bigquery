239f318dd4c4339228daa086c43fd092ffe1a099
daniel bolivar <daniel.bolivar@crowdcow.com>
2024-10-07 12:42:52 -0500

    jenkins update

--- jenkins_bin/jenkins_run.sh
+++ jenkins_bin/jenkins_run.sh
@@ -3,16 +3,6 @@
 # exit when any command fails
 set -e
 
-echo "Current directory:"
-pwd
-
-echo "Listing all files in /tmp:"
-ls -la /tmp/
-
-echo "Listing /tmp/service_account.json inside the container:"
-ls -l /tmp/service_account.json
-file /tmp/service_account.json
-
 dbt seed --target qa 
 dbt snapshot 
 dbt run --target qa 
