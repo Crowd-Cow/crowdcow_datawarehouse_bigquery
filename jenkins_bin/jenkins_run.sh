c6607fbb2e6b11756f40d0c49fb557ee0cac0f00
daniel bolivar <daniel.bolivar@crowdcow.com>
2024-10-26 18:14:14 -0500

    clone to dev removed

--- jenkins_bin/jenkins_run.sh
+++ jenkins_bin/jenkins_run.sh
@@ -8,4 +8,3 @@ dbt snapshot
 dbt run --target qa 
 dbt test --target qa
 dbt clone --select models/ --state target/ --target prod --target-path target_clone --full-refresh
-dbt clone --select models/ --state target/ --target dev --target-path target_clone --full-refresh
