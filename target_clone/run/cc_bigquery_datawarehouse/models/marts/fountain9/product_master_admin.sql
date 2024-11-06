336234f68080c14c0e1a53c7394c9413633a6b26
daniel bolivar <daniel.bolivar@crowdcow.com>
2024-11-06 17:15:22 -0500

    clone visits

---
+++ target_clone/run/cc_bigquery_datawarehouse/models/marts/fountain9/product_master_admin.sql
@@ -0,0 +1,6 @@
+
+
+  create or replace view `panoply-0ef-a098d410468d`.`db_analytics_dev_fountain9`.`product_master_admin`
+  OPTIONS()
+  as select * from `panoply-0ef-a098d410468d`.`qa_fountain9`.`product_master_admin`;
+
