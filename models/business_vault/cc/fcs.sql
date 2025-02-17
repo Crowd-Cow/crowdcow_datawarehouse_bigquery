7d440b33eb1478cfd48c71925c64a04f4517bebc
daniel bolivar <daniel.bolivar@crowdcow.com>
2025-02-17 11:46:37 -0500

    Fix added to fcs to include all and new fcs

--- models/business_vault/cc/fcs.sql
+++ models/business_vault/cc/fcs.sql
@@ -5,7 +5,7 @@ fc as ( select * from {{ ref('stg_cc__fcs') }} )
 select
     *
     ,case
-        when fc_id in (4,5) then 'CROWD COW FC'
+        when fc_id in (4,5,17,18) then 'CROWD COW FC'
         when fc_id = 10 then 'DROP SHIP'
         else 'OTHER'
     end as fc_type
--- models/staging/cc/stg_cc__fcs.sql
+++ models/staging/cc/stg_cc__fcs.sql
@@ -1,6 +1,6 @@
 with source as (
 
-    select * from {{ ref('fcs_ss') }} where not _fivetran_deleted
+    select * from {{ ref('fcs_ss') }} where (_fivetran_deleted is null or _fivetran_deleted = false)
 
 ),
 
