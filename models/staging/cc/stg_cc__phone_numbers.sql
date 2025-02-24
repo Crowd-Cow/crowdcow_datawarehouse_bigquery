b3ec6fa95b2ad2725984dcdfdc85acc39fc28311
daniel bolivar <daniel.bolivar@crowdcow.com>
2025-02-24 16:27:30 -0500

    changed data type in staging phone number table

--- models/staging/cc/stg_cc__phone_numbers.sql
+++ models/staging/cc/stg_cc__phone_numbers.sql
@@ -11,7 +11,7 @@ renamed as (
     ,{{ clean_strings('phone_type') }} as phone_type
     ,{{ clean_strings('phone_number') }} as phone_number
     ,token as phone_number_token
-    ,allow_sms as does_allow_sms
+    ,if(allow_sms = 1, true, false) as does_allow_sms
     ,created_at as created_at_utc
     ,updated_at as updated_at_utc
     ,validated_at as validated_at_utc
