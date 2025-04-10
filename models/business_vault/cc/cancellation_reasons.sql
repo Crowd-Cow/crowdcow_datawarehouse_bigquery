2e427e2678593fa01fa51e3e0c12a2ba5aa02ba6
daniel bolivar <daniel.bolivar@crowdcow.com>
2025-04-10 11:47:22 -0500

    ProsperStack cancellation reasons included

--- models/business_vault/cc/cancellation_reasons.sql
+++ models/business_vault/cc/cancellation_reasons.sql
@@ -2,8 +2,20 @@ with
 
 cancel_events as ( select * from {{ ref('stg_prosperstack__cancellation_reasons') }}  )
 
-
 select
-    *
+    event_id
+    ,created_at
+    ,event_name
+    ,{{ clean_strings('question_1') }} as question_1
+    ,{{ clean_strings('sentiment_1') }} as sentiment_1
+    ,{{ clean_strings('reason_1') }} as reason_1
+    ,{{ clean_strings('question_2') }} as question_2
+    ,{{ clean_strings('sentiment_2') }} as sentiment_2
+    ,{{ clean_strings('reason_2') }} as reason_2
+    ,{{ clean_strings('offers_presented') }} as offers_presented
+    ,{{ clean_strings('status') }} as status
+    ,user_token
+
+    
 from cancel_events 
 qualify row_number() over ( partition by user_token order by created_at desc ) = 1
--- models/staging/prosperstack/stg_prosperstack__cancellation_reasons.sql
+++ models/staging/prosperstack/stg_prosperstack__cancellation_reasons.sql
@@ -7,12 +7,18 @@ source as ( select * from {{ source('prosperstack', 'prosperstack_events') }} )
         id as event_id
         ,created_at
         ,event as event_name
-        ,JSON_EXTRACT_SCALAR(JSON_EXTRACT_ARRAY(data,'$.answers')[SAFE_OFFSET(0)], '$.question.text') as question
-        ,JSON_EXTRACT_SCALAR(data, '$.cancel_reason.text') as cancelation_reason
+        ,JSON_EXTRACT_SCALAR(JSON_EXTRACT_ARRAY(data,'$.answers')[SAFE_OFFSET(0)], '$.question.text') as question_1
+        ,JSON_EXTRACT_SCALAR(JSON_EXTRACT_ARRAY(data,'$.answers')[SAFE_OFFSET(0)], '$.sentiment') as sentiment_1
+        ,JSON_EXTRACT_SCALAR(JSON_EXTRACT_ARRAY(data,'$.answers')[SAFE_OFFSET(0)], '$.value[0].text') as reason_1
+        ,JSON_EXTRACT_SCALAR(JSON_EXTRACT_ARRAY(data,'$.answers')[SAFE_OFFSET(1)], '$.question.text') as question_2
+        ,JSON_EXTRACT_SCALAR(JSON_EXTRACT_ARRAY(data,'$.answers')[SAFE_OFFSET(1)], '$.sentiment') as sentiment_2
+        ,JSON_EXTRACT_SCALAR(JSON_EXTRACT_ARRAY(data,'$.answers')[SAFE_OFFSET(1)], '$.value') as reason_2
+        ,JSON_EXTRACT_SCALAR(JSON_EXTRACT_ARRAY(data,'$.offers_presented')[SAFE_OFFSET(0)], '$.name') as offers_presented
+        ,JSON_EXTRACT_SCALAR(data, '$.status') as status
         ,JSON_EXTRACT_SCALAR(data, '$.subscriber.platform_id') as user_token
     from raw_prosperstack.prosperstack_events
     where event = 'flow_session_completed'
     order by created_at desc
 ) 
 
-select * from renamed where question is not null
+select * from renamed where question_1 is not null
