8885baf4c4568a9e7cd65120989eb64df6785304
daniel bolivar <daniel.bolivar@crowdcow.com>
2025-07-14 08:51:47 -0500

    fc_postal_codes flags fixed

--- models/staging/cc/stg_cc__fc_postal_codes.sql
+++ models/staging/cc/stg_cc__fc_postal_codes.sql
@@ -17,8 +17,8 @@ source as ( select * from {{ source('cc', 'fc_postal_codes') }} where __deleted 
     ,add_additional_ice_in_lbs
     --,delay_delivery_until
     --, cents_to_usd('override_shipping_fee_cents') }} as override_shipping_fee_usd
-    ,sunday_delivery as has_sunday_delivery
-    ,saturday_delivery as has_saturday_delivery
+    ,if(sunday_delivery = 1, true, false) as has_sunday_delivery
+    ,if(saturday_delivery = 1,true, false) as has_saturday_delivery
     ,{{ cents_to_usd('free_shipping_threshold_cents') }} as free_shipping_threshold_usd
   from source
 )
